7e1ae70c96ebe3e0d7b09b2f67ab7b9bd391e3c1
Meli <faistuda3000@gmail.com>
2024-09-24 16:09:06 -0300

    adición de archivo .js actualizado y primeras cards con querySelector

---
+++ main.js
@@ -0,0 +1,14 @@
+const cards = document.querySelectorAll('.card');
+const inner_text = document.querySelectorAll('.card');
+
+cards[0].querySelector('.card-title').textContent = "Brooklyn Nine-Nine";
+cards[0].querySelector('.card-text').textContent = "Los detectives Jake Peralta, Amy Santiago, Rosa Diaz, Charles Boyle y el sargento Terry Jeffords forman una simpática y poco convencional brigada, pero todo cambia tras la llegada del nuevo jefe, el inflexible Raymond Holt.";
+
+cards[1].querySelector('.card-title').textContent = "Derry Girls";
+cards[1].querySelector('.card-text').textContent = "Las vivencias de un grupo de amigas adolescentes en la década de los 90 en Derry, Irlanda del Norte. Una edad convulsa en una época y un lugar difíciles.";
+
+cards[2].querySelector('.card-title').textContent = "Anne With An E";
+cards[2].querySelector('.card-text').textContent = "En la década de 1890, una niña huérfana de 13 años es enviada por error a vivir con hermanos mayores a la Isla del Príncipe Eduardo.";
+
+
+console.log
